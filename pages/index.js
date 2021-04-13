e1b894754031467fc6541a41789ca9dc36449347
bhawayb <bhawaybangiahsr@gmail.com>
2021-04-13 14:38:24 +0530

    Corousel Component

--- pages/index.js
+++ pages/index.js
@@ -3,82 +3,23 @@ import styles from "../styles/Home.module.css";
 import CountUp from "react-countup";
 import Rowimg from "../Component/Rowimgs";
 import Card from "../Component/Cards";
+import Corousel from "../Component/Corousels";
 export default function Home() {
   return (
     <>
       <Head>
         <title>CAPERBERRY</title>
         <link rel="icon" href="/favicon.ico" />
-
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"
         />
-
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
       </Head>
       <img className={styles.plants} src="/images/plants.jpg"></img>
-      {/* <div
-        id="carouselExampleSlidesOnly"
-        class="carousel slide"
-        data-ride="carousel"
-      >
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img
-              class="d-block w-100  img-fluid"
-              src="/images/plants.jpg"
-              alt="First slide"
-            />
-          </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" src="/" alt="Second slide" />
-          </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" src="/" alt="Third slide" />
-          </div>
-        </div>
+      {/* <div>
+        <Corousel></Corousel>
       </div> */}
-      {/* <div
-        id="carouselExampleControls"
-        class="carousel slide"
-        data-ride="carousel"
-      >
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img
-              class="d-block w-100"
-              src="/images/plants.jpg"
-              alt="First slide"
-            />
-          </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" src="" alt="Second slide" />
-          </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" src="" alt="Third slide" />
-          </div>
-        </div>
-        <a
-          class="carousel-control-prev"
-          href="#carouselExampleControls"
-          role="button"
-          data-slide="prev"
-        >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a
-          class="carousel-control-next"
-          href="#carouselExampleControls"
-          role="button"
-          data-slide="next"
-        >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div> */}
-
       <div className="row">
         <h2 className={styles.why}>WHY CAPERBERRY?</h2>
       </div>
